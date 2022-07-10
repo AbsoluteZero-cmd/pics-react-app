b12e7a38f91e8ee969763efe2768b7092be26001
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2022-07-10 22:38:33 +0600

    error fix v1

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,21 +3,30 @@ import React from 'react';
 class App extends React.Component {
     state = { term: '' };
 
-    componentDidUpdate() {
+    constructor() {
+        super();
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
         console.log(this.state.term);
     }
 
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={this.onFormSubmit}>
                     <div className='field'>
                         <label>Image search</label>
                         <input
                             type='text'
                             value={this.state.term}
                             onChange={(e) =>
-                                this.setState({ term: e.target.value })
+                                this.setState({
+                                    term: e.target.value,
+                                })
                             }
                         />
                     </div>
